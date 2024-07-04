a5fe1289ab147ac3adbb8bf1ffdf60249e0cc502
nithyarg <nithyaasbsc@gmail.com>
2024-07-04 21:06:19 +0530

    1h:24 exercise image missing error

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dow';
+import { Button, Stack, Typography } from '@mui/material';
+
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+        <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"/>
+
+    </Link>
+  )
+}
+
+export default ExerciseCard
