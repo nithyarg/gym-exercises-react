139d52001b32a4e201f01032d35fdb3838e9da4c
nithyarg <nithyaasbsc@gmail.com>
2024-07-05 20:58:27 +0530

    1h:39m

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,7 +7,35 @@ import ExerciseCard from './ExerciseCard';
 
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-  console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9 ;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice (indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+     setCurrentPage(value);
+
+     window.scrollTo({top: 1800, behavior:'smooth'})
+  }
+
+  useEffect(() =>{
+    const fetchExerciseData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all'){
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=10000',
+          exerciseOptions)
+      } else {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}',
+          exerciseOptions
+        );
+      }
+      setExercises(exercisesData); 
+    }
+      fetchExerciseData();
+  },[bodyPart]);
 
   return (
      <Box id="exercises"
@@ -23,8 +51,20 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
             {exercises.map ((exercise,index) =>(
               <ExerciseCard key={index} exercise={exercise}/>
             ))}
+            </Stack>
+            <Stack mt="100px" alignItems="center">
+              {exercises.length > 9 && (
+                <Pagination
+                color="standard"
+                shope="rounded"
+                defaultPages={1}
+                count={Math.ceil(exercises.length/exercisesPerPage)}
+                page={currentPage}
+                onChange={paginate}
+                size='large'/>
+              )}
 
-          </Stack>
+            </Stack>
 
 
      </Box>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,9 @@ import HeroBanner from "../components/HeroBanner";
 const Home = () => {
   const [bodyPart, setBodyPart] =useState('all')
   const [exercises, setExercises] = useState([]);
+
+  console.log(bodyPart)
+
   return (
    <Box>
      <HeroBanner />
