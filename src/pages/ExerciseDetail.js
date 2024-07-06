c385423ba58f8d50d4cb8ad54837ca28e13d06fc
nithyarg <nithyaasbsc@gmail.com>
2024-07-06 22:16:39 +0530

    1h:57m image error

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { useParams} from 'react-router-dom' ;
 import {Box} from '@mui/material'
 
-import {exercisesOptions, fetchData} from '../utils/fetchData';
+import {exerciseOptions, fetchData} from '../utils/fetchData';
 
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
@@ -10,9 +10,23 @@ import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } =useParams();
+
+  useEffect(()  =>{
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise${id}`, exerciseOptions);
+      console.log({exerciseDetailData});
+      setExerciseDetail(exerciseDetail);
+
+    }
+  }, [id]);
+  
   return (
     <Box>
-      <Detail/>
+      <Detail exerciseDetail={exerciseDetail}/>
       <ExerciseVideos/>
       <SimilarExercises/>
     </Box>
